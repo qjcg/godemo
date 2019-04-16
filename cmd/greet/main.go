c5bd1a175ecf22fa22444f21cae548847db4eb57
John Gosset <john@gossetx.com>
2019-04-16 14:39:41 -0400

    Add godemo pkg & greet command

---
+++ cmd/greet/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/qjcg/godemo"
+)
+
+func main() {
+	godemo.Greet()
+}
---
+++ hello.go
@@ -0,0 +1,10 @@
+// The godemo package exports interesting functions for having a great Tuesday
+// afternoon.
+package godemo
+
+import "fmt"
+
+// Greet function returns a friendly greeting.
+func Greet() {
+	fmt.Println("Howdy!")
+}
